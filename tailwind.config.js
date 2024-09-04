35c16e3a76456248d3049c74ffb7a3dcba5c383b
Robin <robincherpitel@gmail.com>
2024-09-04 19:14:40 +0200

    Update tailwind.config.js
    got rid of flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,css,ts}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
 
